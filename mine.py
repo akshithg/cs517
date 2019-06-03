40b4ed6ebe12946f7555efb3673c0033f161cb8a
Akshith Gunasekaran <akshith.573@gmail.com>
2019-06-03 05:40:30 -0700

    block 2 int array

--- mine.py
+++ mine.py
@@ -1,31 +1,45 @@
 #!/usr/bin/env python2
 
 import hashlib
+import json
+import sys
 
-# block 125552
-# values are in little endian format
-block = {
-    'v': '01000000',
-    'prev': '81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000',
-    'merkle': 'e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b',
-    'time': 'c7f5d74d',
-    'target': 'f2b9441a',
-    'nonce': '42a14695'
-}
 
-header_hex = block['v'] + block['prev'] + block['merkle'] + block['time'] \
+def main():
+    if(len(sys.argv) < 2):
+        print('Usage: {} block.json'.format(sys.argv[0]))
+        quit()
+
+    with open(sys.argv[1]) as json_file:
+        block = json.load(json_file)
+
+    print('\n-- header data --')
+    for k in block.keys():
+        print('{} : {}'.format(k, block[k]))
+
+    print('\n-- header (little endian) --')
+    print('version + prev + merkle + time + target + nonce')
+    header_hex = block['version'] + block['prev'] + block['merkle'] + block['time'] \
     + block['target'] + block['nonce']
+    print(header_hex)
 
-# hex to bin
-header_bin = header_hex.decode('hex')
+    print('\n-- header to int array --')
+    for i in xrange(0,len(header_hex),8):
+        print(int(header_hex[i:i+8],16))
 
-# sha256(sha256(x))
-hash = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
+    # hex to bin
+    header_bin = header_hex.decode('hex')
+    # sha256(sha256(x))
+    hash = hashlib.sha256(hashlib.sha256(header_bin).digest()).digest()
+    # bin to hex
+    hash.encode('hex_codec')
 
-# bin to hex
-hash.encode('hex_codec')
-# '1dbd981fe6985776b644b173a4d0385ddc1aa2a829688d1e0000000000000000'
+    print('\n-- hash little endian --')
+    print(hash[:].encode('hex_codec'))
 
-# little endian to big endian
-print(hash[::-1].encode('hex_codec'))
-# '00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d'
+    # little endian to big endian
+    print('\n-- hash big endian --')
+    print(hash[::-1].encode('hex_codec'))
+
+if __name__== "__main__":
+  main()
